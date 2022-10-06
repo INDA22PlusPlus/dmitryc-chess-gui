2358c831b935fbc1925620233a4a9d02a2b4d237
Dmitry Chirin <chirin.dima@gmail.com>
2022-10-06 19:21:40 +0200

    try polling but fail

--- src/networking.rs
+++ src/networking.rs
@@ -5,6 +5,7 @@ use std::{
 use dynchess_lib::ChessBoard;
 use crate::networking_protobuf::{c2s_message, C2sConnectRequest, C2sMessage, s2c_message, S2cConnectAck, S2cMessage};
 use crate::networking_protobuf::c2s_message::Msg::ConnectRequest;
+use crate::networking_protobuf::s2c_message::Msg::ConnectAck;
 use prost::Message;
 use polling::{Event, Poller};
 
@@ -21,24 +22,22 @@ pub enum ConnectionType {
 }
 
 pub struct Networking {
-    // Logic
-    // pub from: u8,
-    // pub to: u8,
     pub state: State,
 
     // Networking
+    pub socket: TcpStream,
     pub poller: Poller,
 
     connection: ConnectionType,
 
     pub game_id: u64,
-    pub poller_key: u64,
+    pub poller_key: usize,
 }
 
 impl Networking {
     pub(crate) fn new() -> Networking {
         let game_id = 1 as u64;
-        let poller_key = game_id.clone();
+        let poller_key = game_id.clone() as usize;
         // A stream and a boolean indicating whether or not the program is a host or a client
         let (stream, client, connection_type) = {
             let mut args = std::env::args();
@@ -57,7 +56,7 @@ impl Networking {
                     let listener = TcpListener::bind("127.0.0.1:1337").unwrap();
                     (listener.incoming().next().unwrap().unwrap(), false, ConnectionType::host(
                         S2cMessage {
-                            msg: Some(s2c_message::Msg::ConnectAck(S2cConnectAck {
+                            msg: Some(ConnectAck(S2cConnectAck {
                                 success: false,
                                 game_id: Some(game_id),
                                 starting_position: None,
@@ -72,7 +71,7 @@ impl Networking {
                     let ip = args.next().expect("Expected ip address after --client");
                     let stream = TcpStream::connect(ip).expect("Failed to connect to host");
                     (stream, true, ConnectionType::client(C2sMessage{
-                        msg: Some(c2s_message::Msg::ConnectRequest(C2sConnectRequest{
+                        msg: Some(ConnectRequest(C2sConnectRequest{
                             game_id,
                             spectate: false
                         }))
@@ -88,15 +87,18 @@ impl Networking {
             .set_nonblocking(true)
             .expect("Failed to set stream to non blocking");
 
+        let poller = Poller::new()
+            .expect("Couldn't create a Poller");
+        poller.add(&stream, Event::readable(poller_key))
+            .expect("Couldn't add to poller");
+
         Networking {
-            // from: if client { 0 } else { 63 },
-            // to: if client { 63 } else { 0 },
-            // Host starts playing and the client waits
             state: if client {
                 State::WaitingForOpponent
             } else {
                 State::Playing
             },
+            socket: stream,
             poller,
             connection: connection_type,
             game_id,
@@ -106,25 +108,26 @@ impl Networking {
 
     /// Checks if a move packet is available in returns the new positions otherwise it returns none
     pub fn receive_move_packet(&mut self) -> Option<[u8; 512]> {
-        events.clear();
-        poller.wait(&mut events, None)?;
+        let mut events = vec![];
+        self.poller.wait(&mut events, None).expect("Couldn't receive events with None timeout");
 
-        let packet;
+        let mut packet = None;
         for ev in &events {
-            if ev.key == key {
+            if ev.key == self.poller_key {
                 // Perform a non-blocking accept operation.
-                socket.accept()?;
+                self.socket.accept()
+                    .except("Couldn't accept a stream");
                 // Set interest in the next readability event.
-                poller.modify(&socket, Event::readable(self.poller_key))?;
+                self.poller.modify(&self.socket, Event::readable(self.poller_key))
+                    .expect("Couldn't modify the poller");
 
-                let mut buf = [0u8; 512];
-                let packet  = match self.poller.read(&mut buf) {
-                    Ok(_) => Some(buf),
-                    Err(e) => match e.kind() {
-                        std::io::ErrorKind::WouldBlock => None,
-                        _ => panic!("Error: {}", e),
-                    },
-                };
+                // packet = match self.stream.read(&mut packet) {
+                //     Ok(_) => Some(packet),
+                //     Err(e) => match e.kind() {
+                //         std::io::ErrorKind::WouldBlock => None,
+                //         _ => panic!("Error: {}", e),
+                //     },
+                // };
             }
         }
 
@@ -133,8 +136,6 @@ impl Networking {
 
     /// Sends a move packet of the current position and sets the state to waiting
     pub fn send_move_packet(&mut self) {
-        // self.from = from;
-        // self.to = to;
         let mut buf = match self.connection.clone() {
             ConnectionType::host(host) => {
                 host.encode_to_vec()
@@ -148,7 +149,6 @@ impl Networking {
             .write(&buf)
             .expect("Failed to send move packet");
         self.state = State::WaitingForOpponent;
-        // println!("Packet send: {:?}", buf);
     }
 
     pub fn update(&mut self) {
